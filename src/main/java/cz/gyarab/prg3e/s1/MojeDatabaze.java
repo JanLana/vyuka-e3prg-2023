547978efd634df7b4514bc1c5515f7dce8aec389
Jan Lana <jan.lana@gyarab.cz>
2024-03-08 09:29:51 +0100

    dnesni hodina

--- src/main/java/cz/gyarab/prg3e/s1/MojeDatabaze.java
+++ src/main/java/cz/gyarab/prg3e/s1/MojeDatabaze.java
@@ -1,26 +1,47 @@
 package cz.gyarab.prg3e.s1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class MojeDatabaze {
     public static void main(String[] args) {
+        String[] slova = { "nula", "jedna", "dva", "tri", "ctyri", "pet"};
+
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
-            try(Statement stmt = conn.createStatement()) {
-                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS cislo (n INT NOT NULL)");
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(
+                        "CREATE TABLE IF NOT EXISTS slova (" +
+                                "n INT NOT NULL," +
+                                "popis VARCHAR NOT NULL" +
+                                ")");
             }
 
-
-            for (int i = 1; i < 110; i++) {
+/*
+            for (int i = 0; i < slova.length; i++) {
                 try (Statement stmt = conn.createStatement()) {
-                    stmt.executeUpdate("INSERT INTO cislo VALUES (" + i + ")");
+                    String sql = "INSERT INTO slova VALUES (" + i + ", '" + slova[i] + "')";
+                    System.out.println(sql);
+                    stmt.executeUpdate(sql);
                 }
             }
+*/
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+            Scanner sc = new Scanner(System.in);
+            for (; ; ) {
+                int cislo = sc.nextInt();
+                try (Statement stmt = conn.createStatement()) {
+                    ResultSet rs = stmt.executeQuery("SELECT popis FROM slova WHERE n=" + cislo);
+                    while (rs.next()) {
+                        String p = rs.getString(2);
+                        int n = rs.getInt(1);
+
+                        System.out.println("radek: " + n + " = " + p);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
